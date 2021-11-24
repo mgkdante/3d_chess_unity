0dad2fbb92ec9e928353fdd25a4881ac2cb39dd7
Larfleeze2828 <Liambuckthompson@gmail.com>
2021-11-24 16:30:13 -0500

    Added required scripts, mostly empty

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(IObjectTweener))]
+[RequireComponent(typeof(MaterialSetter))]
 public class Piece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private MaterialSetter materialSetter;
+
+    public Board board { protected get; set; }
+
+    public Vector2Int occupiedSquare { get; set; }
+
+    public TeamColor team { get; set; }
+   
+    public bool hasMoved { get; private set; }
+
+    public List<Vector2Int> availableMoves;
+
+    private IObjectTweener tweener;
+
+    public abstract List<Vector2Int> SelectAvailableSquares();
+
+    private void Awake()
     {
-        
+        availableMoves = new List<Vector2Int>();
+        tweener = GetComponent<IObjectTweener>();
+        materialSetter = GetComponent<materialSetter>();
+        hasMoved = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetMaterial(Material material)
     {
-        
+        materialSetter.SetSingleMaterial(material);
+    }
+
+    public bool IsFromSameTeam(Piece piece)
+    {
+
+    }
+
+    public bool CanMoveTo(Vector2Int coords)
+    {
+
     }
 }
