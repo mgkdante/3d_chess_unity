9cd361e22374d8da776e95116bc37b410b0598b7
Larfleeze2828 <Liambuckthompson@gmail.com>
2021-11-25 15:23:25 -0500

    Added prefabs to the board on game start

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-<<<<<<< HEAD
-=======
     [SerializeField] private Transform bottomLeftSquareTransform;
     [SerializeField] private float squareSize;
 
    internal Vector3 CalculatePositionFromCoords(Vector2Int coords)
     {
         return bottomLeftSquareTransform.position + new Vector3(coords.x * squareSize, 0f, coords.y * squareSize);
->>>>>>> 691a2ae15040a47ac61b8b6c62bfd3643064e2f0
     }
 }
